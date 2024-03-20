277aae90f2435eb2ca1655d91ac22851b8a0f690
Idris TAKERBOUST <idris@takerboust.fr>
2024-03-20 18:03:13 +0100

    remove unnecessary double checks & dead code

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"context"
 	"urlShortener/database"
 	"urlShortener/router"
 )
-
-var ctx = context.TODO()
 
 func main() {
 	dbConnection, err := database.NewMongoDBConnection("mongodb://localhost:27017", "urlshortener")
 	if err != nil {
 		panic("Impossible d'initialiser la base de données MongoDB: " + err.Error())
-	}
-
-	if err := dbConnection.Client.Ping(ctx, nil); err != nil {
-		panic("Impossible de se connecter à la base de données MongoDB: " + err.Error())
 	}
 
 	database.Connection = dbConnection

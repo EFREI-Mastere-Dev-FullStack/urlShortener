63f2033ba1983208636263ec1b92338a8089f406
Idris TAKERBOUST <idris@takerboust.fr>
2024-03-27 19:33:56 +0100

    feat: generate slug

--- controller/controller.go
+++ controller/controller.go
@@ -3,11 +3,14 @@ package controller
 import (
 	"context"
 	"github.com/gin-gonic/gin"
+	"math/rand"
 	"net/http"
 	"os"
 	"urlShortener/database"
 	"urlShortener/model"
 )
+
+const charBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
 
 var BaseURL = os.Getenv("BASE_URL")
 
@@ -23,7 +26,9 @@ func ShortenURL(c *gin.Context) {
 		return
 	}
 
-	// TODO: Generer un slug [faudrait faire attention a ce que le slug soit unique en BDD]
+	// TODO: Amelioration: Verifier l'unicité du slug en base de données
+	url.ShortenedSlug = generateSlug(6)
+
 	// TODO: Interdire les dates d'expiration dans le passé [url.ExpirationDate]
 
 	db := database.Connection
@@ -35,4 +40,13 @@ func ShortenURL(c *gin.Context) {
 	}
 
 	c.HTML(http.StatusOK, "shorten.html", gin.H{"BaseURL": BaseURL, "url": url})
+}
+
+// private function
+func generateSlug(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = charBytes[rand.Intn(len(charBytes))]
+	}
+	return string(b)
 }

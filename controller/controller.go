50be6df584d65f7bfe4e90a70b28d3b4365f52f1
Idris TAKERBOUST <idris@takerboust.fr>
2024-04-25 12:19:57 +0200

    refactoring: move the database responsibility into model instead of controller

--- controller/controller.go
+++ controller/controller.go
@@ -1,19 +1,12 @@
 package controller
 
 import (
-	"context"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
-	"math/rand"
 	"net/http"
 	"os"
 	"time"
-	"urlShortener/database"
 	"urlShortener/model"
 )
-
-const charBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
 
 var BaseURL = os.Getenv("BASE_URL")
 
@@ -23,15 +16,12 @@ func IndexPage(c *gin.Context) {
 
 func ShortenURL(c *gin.Context) {
 	var url model.URL
-
 	if err := c.ShouldBind(&url); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	db := database.Connection
-
-	err := createURL(db, &url)
+	err := url.Save()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -44,12 +34,8 @@ func RedirectURL(c *gin.Context) {
 	slug := c.Param("slug")
 
 	var url model.URL
-	db := database.Connection
+	err := url.DecodeSlugAndCount(slug)
 
-	filter := bson.M{"$or": []bson.M{{"shortened_slug": slug}, {"alias": slug}}}
-	update := bson.M{"$inc": bson.M{"count": 1}}
-
-	err := db.Database.Collection("urls").FindOneAndUpdate(context.Background(), filter, update).Decode(&url)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "URL not found"})
 		return
@@ -61,49 +47,4 @@ func RedirectURL(c *gin.Context) {
 	}
 
 	c.Redirect(http.StatusMovedPermanently, url.Original)
-}
-
-// private function
-func createURL(db *database.MongoDBConnection, url *model.URL) error {
-	if url.Alias != "" {
-		filter := bson.M{"$or": []bson.M{{"shortened_slug": url.Alias}, {"alias": url.Alias}}}
-		err := db.Database.Collection("urls").FindOne(context.Background(), filter).Err()
-		if err == nil {
-			return fmt.Errorf("alias already in use")
-		}
-	}
-
-	url.ShortenedSlug = generateSlug(db, 6)
-	url.CreatedAt = time.Now().Local()
-	url.Count = 0
-
-	if !url.ExpiredAt.IsZero() && url.ExpiredAt.Before(url.CreatedAt) {
-		return fmt.Errorf("expiration date can't be in the past")
-	}
-
-	_, err := db.Database.Collection("urls").InsertOne(context.Background(), url)
-	if err != nil {
-		return fmt.Errorf("error inserting url into database: %v", err)
-	}
-
-	return nil
-}
-
-func generateSlug(db *database.MongoDBConnection, n int) string {
-	var slug string
-	for {
-		b := make([]byte, n)
-		for i := range b {
-			b[i] = charBytes[rand.Intn(len(charBytes))]
-		}
-		slug = string(b)
-
-		filter := bson.M{"shortened_slug": slug}
-		err := db.Database.Collection("urls").FindOne(context.Background(), filter).Err()
-		if err != nil {
-			break
-		}
-	}
-
-	return slug
 }
--- model/url.go
+++ model/url.go
@@ -1,8 +1,13 @@
 package model
 
 import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"math/rand"
 	"time"
+	"urlShortener/database"
 )
 
 type URL struct {
@@ -13,4 +18,63 @@ type URL struct {
 	ExpiredAt     time.Time          `bson:"expired_at,omitempty" time_format:"2006-01-02"`
 	CreatedAt     time.Time          `bson:"created_at"`
 	Count         int                `bson:"count"`
+}
+
+const charBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
+
+func (url *URL) Save() error {
+	var db = database.Connection
+
+	if url.Alias != "" {
+		filter := bson.M{"$or": []bson.M{{"shortened_slug": url.Alias}, {"alias": url.Alias}}}
+		err := db.Database.Collection("urls").FindOne(context.Background(), filter).Err()
+		if err == nil {
+			return fmt.Errorf("alias already in use")
+		}
+	}
+
+	url.ShortenedSlug = generateSlug(db, 6)
+	url.CreatedAt = time.Now().Local()
+	url.Count = 0
+
+	if !url.ExpiredAt.IsZero() && url.ExpiredAt.Before(url.CreatedAt) {
+		return fmt.Errorf("expiration date can't be in the past")
+	}
+
+	_, err := db.Database.Collection("urls").InsertOne(context.Background(), url)
+	if err != nil {
+		return fmt.Errorf("error inserting url into database: %v", err)
+	}
+
+	return nil
+}
+
+func (url *URL) DecodeSlugAndCount(slug string) error {
+	var db = database.Connection
+
+	filter := bson.M{"$or": []bson.M{{"shortened_slug": slug}, {"alias": slug}}}
+	update := bson.M{"$inc": bson.M{"count": 1}}
+
+	err := db.Database.Collection("urls").FindOneAndUpdate(context.Background(), filter, update).Decode(&url)
+
+	return err
+}
+
+func generateSlug(db *database.MongoDBConnection, n int) string {
+	var slug string
+	for {
+		b := make([]byte, n)
+		for i := range b {
+			b[i] = charBytes[rand.Intn(len(charBytes))]
+		}
+		slug = string(b)
+
+		filter := bson.M{"shortened_slug": slug}
+		err := db.Database.Collection("urls").FindOne(context.Background(), filter).Err()
+		if err != nil {
+			break
+		}
+	}
+
+	return slug
 }
